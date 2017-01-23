464a6838b21fc4d7ccb9c1f212e6719a5e074116
Roberto Guillén <robertoguillen@me.com>
2017-01-23 03:18:53 +0100

    Added suppliers to generate curves like a Spirograph could do (Epitrochoid curves and Hypotrochoid curves). So cute if they're well configured :3

---
+++ library/src/main/java/com/devcexx/libtrails/suppliers/HypotrochoidSupplier.java
@@ -0,0 +1,37 @@
+package com.devcexx.libtrails.suppliers;
+
+import com.devcexx.libtrails.Particle;
+import com.devcexx.libtrails.Vector3;
+
+/**
+ * Class that provides an Hypotrochoid trail.
+ */
+public class HypotrochoidSupplier extends SpirographSupplier {
+
+    /**
+     * Builds a new epitrochoid trail.
+     * @param particle The particle that will be spawned.
+     * @param delta The variation of the angle per drawn point.
+     * @param directorRadius The radius of the fixed circumference.
+     * @param rollingRadius The radius of the rolling circumference.
+     * @param hdist The distance between the center of the rolling center to
+     *              the point that will be used to draw the curve.
+     * @param appearingInterval the time, in Minecraft ticks, that must elapse
+     *                          between each drawing.
+     */
+    public HypotrochoidSupplier(Particle particle, float delta,
+                                float directorRadius, float rollingRadius,
+                                float hdist, int appearingInterval) {
+        super(particle, delta, directorRadius, rollingRadius, hdist,
+                appearingInterval);
+    }
+
+    @Override
+    protected Vector3 fetchVector(float R, float r, float h, float theta) {
+        return new Vector3(
+                (R - r) * Math.cos(theta) + h * Math.cos(theta * (R - r) / r),
+                0,
+                (R - r) * Math.sin(theta) - h * Math.sin(theta * (R - r) / r)
+        );
+    }
+}
